623a0882798e1eaf45f0627f2271b9f07bd1f318
EyalHadad <eyalhadad1989@gmail>
2022-02-06 11:07:17 +0200

    freez layers in improve one model

--- notebooks/improve_one_model.py
+++ notebooks/improve_one_model.py
@@ -5,18 +5,23 @@ from src.models.param_class import TrainModelParam, EvalModelParam
 
 
 def run_transfer(training_dict, src_org, dst_org, folder_path):
-    t_parm = TrainModelParam(folder_path=folder_path, to_save=False, epochs=200)
+    t_parm = TrainModelParam(folder_path=folder_path, to_save=False)
     e_parm = EvalModelParam(folder_path=folder_path)
     t_parm.data_obj = DataOrg(dst_org).load(datasets=training_dict[dst_org], is_train="train")
     e_parm.data_obj = DataOrg(dst_org).load(datasets=training_dict[dst_org], is_train="test")
     t_parm.src_model_to_load = src_org
     baseline_models = get_models_dict(len(e_parm.data_obj))
-    t_parm.part_train = 500
+    for l in baseline_models['base'].model.layers:
+        print(l.name, l.trainable)
+        l.trainable = False
+    baseline_models['base'].model.get_layer('dense_3').trainable = True
+    baseline_models['base'].model.get_layer('dense_4').trainable = True
+    t_parm.part_train = 100
     baseline_models['base'].train_model(t_parm=t_parm)
     baseline_models['base'].evaluate_model(e_parm=e_parm)
 
 
-def run_improve(src_org='human', dst_org='human'):
+def run_improve(src_org='worm', dst_org='cow'):
     dir_path = list_files(MODELS_OBJECTS_PATH)[-1]
     run_transfer(VS4_REG_DICT, src_org, dst_org, dir_path)
 
